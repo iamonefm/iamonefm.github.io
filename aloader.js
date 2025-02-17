bd5a673c4feaf0beb6ecb5058ff88bb9966c2716
fM <iamonefm@gmail.com>
2025-02-17 21:57:39 +0300

    Update aloader.js

--- aloader.js
+++ aloader.js
@@ -49,7 +49,7 @@
             justify-items: center;
             position: relative;
             width: 100%;
-            height: 300px;
+            height: 400px;
             }
   .ani_svg {
             width: 60%;
