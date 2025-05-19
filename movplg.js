c9f8e79b7249a5481d8dcb1bc8cfbec17b133ee1
fM <iamonefm@gmail.com>
2025-05-19 14:28:39 +0300

    Create movplg.js

---
+++ movplg.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+    function movplg(src) {
+
+    const originalFunc = Lampa.ParentalControl.personal;
+    Lampa.ParentalControl.personal = function(...args) {
+        
+        const result = originalFunc.apply(this, args);
+        
+        setTimeout(() => {
+            const $parent = $('[data-component="plugins"]').parent();
+            $('[data-component="plugins"]').prependTo($parent);
+        }, 50);
+        
+        return result;
+    };
+
+    }
+
+if (window.appready) {
+    movplg();
+}
+else {
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type == 'ready') {
+            movplg();
+        }
+    });
+}
+
+})
+();
